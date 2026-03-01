fe195106867d1b1013a388d26fd6dd4008429345
Ryan <123142178+MrRyanPerson@users.noreply.github.com>
2026-03-01 19:30:10 +0000

    Add r2 bucket

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 let Parser = require('rss-parser');
 
+let parser = new Parser();
+
 let bbcRSS = "https://feeds.bbci.co.uk/news/world/rss.xml";
 let abcRSS = "https://abcnews.com/abcnews/topstories";
 
 export default {
 	async scheduled(event, env, ctx) {
-		let bbcResp = await fetch(bbcRSS);
-		let abcResp = await fetch(abcRSS);
-		let bbcSuccessful = bbcResp.ok ? 'success' : 'fail';
-		let abcSuccessful = abcResp.ok ? 'success' : 'fail';
+		let bbcFeed = await parser.parseURL(bbcRSS);
+		let abcFeed = await parser.parseURL(abcRSS);
 
+		console.log(`trigger fired at ${event.cron}: ${bbcFeed.title}, ${abcFeed.title}`);
 
-		console.log(`trigger fired at ${event.cron}: ${bbcSuccessful} , ${abcSuccessful}`);
+		await env.RSS_STORAGE.put(bbcFeed.title + ".json", JSON.stringify(bbcFeed.items));
+		await env.RSS_STORAGE.put(abcFeed.title + ".json", JSON.stringify(abcFeed.items));
 
-		await env.RSS_STORAGE.put("test.txt", "test");
 	},
 };
