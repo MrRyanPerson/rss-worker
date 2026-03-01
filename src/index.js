d66e216a52b62bd22600ccd90d400c745a554b6c
Ryan <123142178+MrRyanPerson@users.noreply.github.com>
2026-03-01 18:58:24 +0000

    Initial Code

---
+++ src/index.js
@@ -0,0 +1,18 @@
+let Parser = require('rss-parser');
+
+let Parser = require('rss-parser');
+
+let bbcRSS = "https://feeds.bbci.co.uk/news/world/rss.xml";
+let abcRSS = "https://abcnews.com/abcnews/topstories";
+
+export default {
+	async scheduled(event, env, ctx) {
+		let bbcResp = await fetch(bbcRSS);
+		let abcResp = await fetch(abcRSS);
+		let bbcSuccessful = bbcResp.ok ? 'success' : 'fail';
+		let abcSuccessful = abcResp.ok ? 'success' : 'fail';
+
+
+		console.log(`trigger fired at ${event.cron}: ${wasSuccessful}`);
+	},
+};
