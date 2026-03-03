b8cce6012f201d721a9e8c3f732ee024ec4e0459
Ryan <123142178+MrRyanPerson@users.noreply.github.com>
2026-03-02 21:41:51 -0800

    Use custom BBC parser for RSS feed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 let Parser = require('rss-parser');
 
 let parser = new Parser();
+let bbcParser = new Parser({
+  customFields: {
+    item: [
+      ['media:thumbnail', 'mediathumbnail'],
+    ]
+  }
+});
 
 let bbcRSS = "https://feeds.bbci.co.uk/news/world/rss.xml";
 let abcRSS = "https://abcnews.com/abcnews/topstories";
 
 export default {
 	async scheduled(event, env, ctx) {
-		let bbcFeed = await parser.parseURL(bbcRSS);
+		let bbcFeed = await bbcParser.parseURL(bbcRSS);
 		let abcFeed = await parser.parseURL(abcRSS);
 
 		console.log(`trigger fired at ${event.cron}: ${bbcFeed.title}, ${abcFeed.title}`);
