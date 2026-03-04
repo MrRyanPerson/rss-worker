2b1eb2f3144d47fe6f3b94324b4b36aecbaed646
Ryan <123142178+MrRyanPerson@users.noreply.github.com>
2026-03-04 13:05:48 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,21 +10,25 @@ let parser = new Parser({
 
 let bbcRSS = "https://feeds.bbci.co.uk/news/world/rss.xml";
 let abcRSS = "https://abcnews.com/abcnews/topstories";
+let wsjRSS = "https://feeds.content.dowjones.io/public/rss/RSSWorldNews";
 
 export default {
 	async scheduled(event, env, ctx) {
 		let bbcFeed = await parser.parseURL(bbcRSS);
 		let abcFeed = await parser.parseURL(abcRSS);
+		let wsjRSS = await parser.parseURL(wsjRSS);
 
 		console.log(`trigger fired at ${event.cron}: ${bbcFeed.title}, ${abcFeed.title}`);
 
 		// delete old feeds before putting new ones
 		await env.RSS_STORAGE.delete("bbcFeed" + ".json");
 		await env.RSS_STORAGE.delete("abcFeed" + ".json");
+		await env.RSS_STORAGE.delete("wsjFeed" + ".json");
 
 		// put new feeds in storage
 		await env.RSS_STORAGE.put("bbcFeed" + ".json", JSON.stringify(bbcFeed.items));
 		await env.RSS_STORAGE.put("abcFeed" + ".json", JSON.stringify(abcFeed.items));
+		await env.RSS_STORAGE.put("wsjFeed" + ".json", JSON.stringify(wsjFeed.items));
 
 		console.log("feeds updated in storage");
 
